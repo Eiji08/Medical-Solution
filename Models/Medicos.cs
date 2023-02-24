f3c4007a262fb28dcf676cdb80888e5c2769be42
Eiji <eijiayukawa@hotmail.es>
2023-02-23 20:47:33 -0400

    entidad medicos

--- Models/Medicos.cs
+++ Models/Medicos.cs
@@ -1,13 +1,13 @@
 ﻿namespace Medical_Solution.Models
 {
-    public class Pacientes
+    public class Medicos
     {
-        public int Id_Paciente { get; set; }
+        public int Id_Medico { get; set; }
         public string Nombre { get; set; }
         public string Apellidos { get; set; }
+        public string Especialidad { get; set; }
         public string Direccion { get; set; }
         public string Email { get; set; }
         public int Telefono { get; set; }
-
     }
 }
